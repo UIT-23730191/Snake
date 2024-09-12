c02187c623affe1103159d3f5bb941b7f38d2627
UIT-23730172-HUY <23730172@ms.uit.edu.vn>
2024-09-12 23:57:35 +0700

    Thêm hàm cộng điểm và reset

--- message.py
+++ message.py
@@ -27,6 +27,15 @@ class ScoreMessage(Message):
         self.goto(0, 260)
         self.write_message(f"Score: {self.score}")
 
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+    def reset(self):
+        self.score = 0
+        self.clear()
+        self.update_scoreboard()
+    
 
 
 class StartMessage(Message):
