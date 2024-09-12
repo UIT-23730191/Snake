1b9bad67824cb256e4ecd2bf1ce4ff63a9199a2c
UIT-23730185 <23730185@ms.uit.edu.vn>
2024-09-12 21:37:53 +0700

    Design movement function

--- snake.py
+++ snake.py
@@ -26,6 +26,30 @@ class Snake:
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+
 
 
 
