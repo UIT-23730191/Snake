bbc73f9f2e4603f9e929af78820e44492df771bb
UIT-23730172-HUY <23730172@ms.uit.edu.vn>
2024-09-13 00:16:09 +0700

    set trạng thái mặc định cho các cài đặt

--- status.py
+++ status.py
@@ -1,5 +1,9 @@
 class Status: 
     def __init__(self): #Khởi tạo đối tượng
+        self.is_start = False
+        self.is_game_over = False
+        self.is_running = False
+
 
     def start(sefl): #Set trạng thái bắt đầu
 
