1a0b4a626bbdece4b927307f79904bb27d91f812
UIT-23730185 <23730185@ms.uit.edu.vn>
2024-09-12 21:33:21 +0700

    Design reset function

--- snake.py
+++ snake.py
@@ -6,6 +6,10 @@ class Snake:
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
+    
+    def reset(self):
+        self.remove_snake()
+        self.__init__()
 
 
 
