5cffc3cb4eec71781a92302a427d91dac2bcd6ac
UIT-23730185 <23730185@ms.uit.edu.vn>
2024-09-13 21:08:14 +0700

    Desgin eat food function

--- event.py
+++ event.py
@@ -1,5 +1,8 @@
 def snake_eat_food(snake, food, score_msg):
-
+    if snake.head.distance(food) < 10:
+        food.refresh()
+        snake.extend()
+        score_msg.increase_score()
 
 
 def snake_hit_wall(snake, game_over_msg, status):
