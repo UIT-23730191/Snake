6645b1d3d1734f5af8948dcd43fd389de22a6305
UIT-23730185 <23730185@ms.uit.edu.vn>
2024-09-13 21:02:54 +0700

    Design refresh function

--- food.py
+++ food.py
@@ -5,5 +5,8 @@ class Food(Turtle):
 
     def __init__(self):
 
-
+def refresh(self):
+    random_x = random.randint(-150, 150)
+    random_y = random.randint(-150, 150)
+    self.goto(random_x, random_y)
 
