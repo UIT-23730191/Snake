cf45dda3207459b10315539cdd4857ef9f1996c6
UIT-23730185 <23730185@ms.uit.edu.vn>
2024-09-13 21:09:13 +0700

    Desgin hit wall function

--- event.py
+++ event.py
@@ -6,7 +6,11 @@ def snake_eat_food(snake, food, score_msg):
 
 
 def snake_hit_wall(snake, game_over_msg, status):
-
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_over_msg.show()
+        status.game_over()
+        return True
+    return False
 
 
 def snake_hit_self(snake, game_over_msg, status):
