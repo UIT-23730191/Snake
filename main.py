c9f374b4debbd46cbd6b707b12f623f5663d799c
UIT-23730172-HUY <23730172@ms.uit.edu.vn>
2024-09-14 18:19:40 +0700

    Thiết lập màn hình trò chơi

---
+++ main.py
@@ -0,0 +1,7 @@
+# Thiết lập màn hình trò chơi
+screen = Screen()
+screen.setup(width=600, height=600)  # Kích thước trò chơi
+screen.bgcolor("white")  # Set Màu nền
+screen.title("SNAKE GAME")  # Set title
+screen.tracer(0)
+
