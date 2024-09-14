195a273c2fc04cd8d152c5bcce5624f1e9be0c3a
UIT-23730172-HUY <23730172@ms.uit.edu.vn>
2024-09-14 18:20:41 +0700

    Khởi tạo các đối trượng

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ screen.bgcolor("white")  # Set Màu nền
 screen.title("SNAKE GAME")  # Set title
 screen.tracer(0)
 
+# Khởi tạo các đối tượng trò chơi
+snake = Snake()
+food = Food()
+score_msg = ScoreMessage()
+start_msg = StartMessage()
+game_over_msg = GameOverMessage()
+status = Status()
