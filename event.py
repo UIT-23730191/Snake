ac7e76e82c93a6b1fb1714fa1a50d6858e4b43fd
UIT-23730185 <23730185@ms.uit.edu.vn>
2024-09-15 12:13:43 +0700

    Add description for event_module

--- event.py
+++ event.py
@@ -1,3 +1,15 @@
+"""
+Module event.py
+
+Module này chứa các hàm xử lý sự kiện cho trò chơi con rắn, bao gồm sự kiện rắn ăn thức ăn, đụng tường, và đụng thân
+mình.
+
+Functions:
+    snake_eat_food(snake, food, score_msg): Xử lý sự kiện khi rắn ăn thức ăn.
+    snake_hit_wall(snake, game_over_msg, status): Xử lý sự kiện khi rắn đụng tường.
+    snake_hit_self(snake, game_over_msg, status): Xử lý sự kiện khi rắn đụng thân mình.
+"""
+
 def snake_eat_food(snake, food, score_msg):
     if snake.head.distance(food) < 10:
         food.refresh()
